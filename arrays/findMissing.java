50fcfab0cb7b2101ee71cc8a9fb44c8c591e87a0
Deepanshu Rautela <deepanshu.mb20@gmail.com>
2023-02-28 23:16:28 +0530

    Update findMissing add Cyclic Sort method

--- arrays/findMissing.java
+++ arrays/findMissing.java
@@ -11,9 +11,35 @@ class GFG {
        for(int i = 0; i < arr.length; i++) xor ^= arr[i];
        return xor;
     }
-	public static void main (String[] args) {
+	
+   public static void main (String[] args) {
 	    int[] arr = {1, 2, 3, 4, 5, 6}; 
 	    int result = findMissingNumber(arr); 
 	    System.out.println(result);
 	}
+}
+
+
+// Function to find the missing number
+// Using Cyclic Sort
+int getMissingNo(int a[], int n)
+{
+    int i = 0;
+    while (i < n) {
+        int correct = a[i] - 1;
+        if (a[i] < n && a[i] != a[correct]) {
+            swap(a[i], a[correct]);
+        }
+        else {
+            i++;
+        }
+    }
+ 
+    for (int i = 0; i < n; i++) {
+        if (i != a[i] - 1) {
+            return i + 1;
+        }
+    }
+ 
+    return n;
 }
