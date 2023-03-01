fc55f390bc0b939bfaab614a3636520f0f33bf34
Deepanshu Rautela <deepanshur@cdac.in>
2023-03-01 11:27:45 +0530

    Add recursive solution

--- arrays/findFirstIncreasingThenDecreasing.java
+++ arrays/findFirstIncreasingThenDecreasing.java
@@ -20,3 +20,19 @@ public class Test{
         System.out.println(findFirstIncreasingThenDecreasing(nums));
     }
 }
+
+// Recursive way 
+// public class Test{
+//     public static int findFirstIncreasingThenDecreasing(int[] nums, int low, int high){
+//         if(low == high) return arr[high]; 
+//         int mid = low + (high - low)/2; 
+//         if(arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) return arr[mid]; 
+//         else if(arr[mid] < arr[mid + 1]) return findFirstIncreasingThenDecreasing(nums, mid + 1, high); 
+//         else findFirstIncreasingThenDecreasing(nums, low, mid - 1); 
+//     }
+
+//     public static void main(String... args){
+//         int[] nums = {8, 10, 20, 80, 100, 200, 400, 500, 3, 2, 1};
+//         System.out.println(findFirstIncreasingThenDecreasing(nums, 0, nums.length - 1));   
+//     }
+// }
