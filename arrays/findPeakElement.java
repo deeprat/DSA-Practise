1aec45b8eaa3f878ce4c8daac7a80f5b9b86a39a
Deepanshu Rautela <deepanshur@cdac.in>
2023-03-02 10:44:42 +0530

    Add findPeakElement.java

---
+++ arrays/findPeakElement.java
@@ -0,0 +1,17 @@
+// https://www.geeksforgeeks.org/find-a-peak-in-a-given-array/
+// Category: Medium
+
+class Solution {
+    int findMaximum(int[] arr, int n) {
+        int l = 0, r = n - 1; 
+        int mid = (l + r) >> 1;
+        while(l <= r){
+            if((mid == 0 || arr[mid] > arr[mid - 1]) && (mid == n - 1 || arr[mid] > arr[mid + 1])) break;
+            else if(mid > 0 && arr[mid - 1] > arr[mid]) r = mid - 1;
+            else l = mid + 1;
+            mid = (l + r) >> 1; 
+        }
+        return arr[mid];
+        
+    }
+}
